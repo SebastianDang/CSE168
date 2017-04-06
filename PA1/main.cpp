f99383f4997a603f21ed9cafffb4af5b88876042
Sebastian Dang <s1dang@ucsd.edu>
2017-04-05 21:59:54 -0700

    Initial Commit

---
+++ PA1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PA1
+//
+//  Created by Sebastian Dang on 4/5/17.
+//  Copyright © 2017 Sebastian Dang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
