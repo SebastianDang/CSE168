9b8f5314f3c43335c1b3d5d3f9998472aa2e2681
Sebastian Dang <s1dang@ucsd.edu>
2017-04-07 19:19:55 -0700

    Cleaned up Triangle calculations

--- Triangle.cpp
+++ Triangle.cpp
@@ -16,48 +16,32 @@ Triangle::Triangle(){
 //Calculate the intersection of the ray to this triangle.
 bool Triangle::Intersect(const Ray &ray, Intersection &hit) const{
     
-    //Ray - r(t) = p + td
-    //q = a + [ alpha * (b-a) ] + [ beta * (c-a) ]
-
-    //Get vertices.
-    Vertex *vertex_a = Vtx[0];
-    Vertex *vertex_b = Vtx[1];
-    Vertex *vertex_c = Vtx[2];
+    //Positions
+    glm::vec3 a = Vtx[0]->Position;
+    glm::vec3 b = Vtx[1]->Position;
+    glm::vec3 c = Vtx[2]->Position;
     
-    //Position
-    glm::vec3 a = vertex_a->Position;
-    glm::vec3 b = vertex_b->Position;
-    glm::vec3 c = vertex_c->Position;
-    
-    //Ray
+    //Rays
     glm::vec3 p = ray.Origin;
     glm::vec3 d = ray.Direction;
     
     //det(M) = -d dot [(b-a) x (c-a)]
-    glm::vec3 negD = -d;
-    glm::vec3 BminusA = b - a;
-    glm::vec3 CminusA = c - a;
-    glm::vec3 BAcrossCA = glm::cross(BminusA, CminusA);
-    float detM = glm::dot(negD, BAcrossCA);
+    float detM = glm::dot(-d, glm::cross(b-a, c-a));
+    if (detM == 0) return false;//Ray is parallel to the plane.
     
     //t = (p-a) dot [(b-a) x (c-a)] / det(M)
-    glm::vec3 PminusA = p - a;
-    float t = glm::dot(PminusA, BAcrossCA);
+    float t = glm::dot(p-a, glm::cross(b-a, c-a)) / detM;
+    if (t <= 0) return false;//Verify t>0
     
     //alpha = -d dot [(p-a) x (c-a)] / det(M)
-    glm::vec3 PAcrossCA = glm::cross(PminusA, CminusA);
-    float alpha = (glm::dot(negD, PAcrossCA)) / detM;
+    float alpha = glm::dot(-d, glm::cross(p-a, c-a)) / detM;
+    if (alpha <= 0) return false;//Verify alpha>0
     
     //beta = -d dot [(b-a) x (p-a)] / det(M)
-    glm::vec3 BAcrossPA = glm::cross(BminusA, PminusA);
-    float beta = (glm::dot(negD, BAcrossPA)) / detM;
+    float beta = glm::dot(-d, glm::cross(b-a, p-a)) / detM;
+    if (beta <= 0) return false;//Verify beta>0
+    if ((alpha + beta) >= 1) return false;//Verify a+b<1
     
-    //Check
-    if (detM == 0) return false;
-    if (alpha < 0) return false;
-    if (beta < 0) return false;
-    if (alpha + beta > 1) return false;
-    if (t < 0) return false;
-    
+    //Else return true.
     return true;
 }
