2b96b705f14e450eedb9694ed64f199d3c3d6bac
Sebastian Dang <s1dang@ucsd.edu>
2017-04-08 10:16:08 -0700

    Fixed instance calculation

--- InstanceObject.cpp
+++ InstanceObject.cpp
@@ -23,7 +23,7 @@ InstanceObject::InstanceObject(Object &obj){
     Matrix = glm::mat4x4(1.0f);
     Inverse = glm::inverse(Matrix);
     
-    Child = &obj;
+    SetChild(obj);
 }
 
 InstanceObject::~InstanceObject(){
@@ -36,14 +36,14 @@ bool InstanceObject::Intersect(const Ray &ray,Intersection &hit){
     //Create ray2 from ray.
     Ray ray2;
     ray2.Origin = glm::vec3(Inverse * glm::vec4(ray.Origin, 1.0f));
-    ray2.Direction = glm::vec3(Inverse * glm::vec4(ray.Direction, 1.0f));
+    ray2.Direction = glm::vec3(Inverse * glm::vec4(ray.Direction, 0.0f));
     
     //Check if the child object intersects.
     if (Child->Intersect(ray2, hit) == false) return false;
     
     //If intersection is true, set the hit position.
     hit.Position = glm::vec3(Matrix * glm::vec4(hit.Position, 1.0f));
-    hit.Normal = glm::vec3(Matrix * glm::vec4(hit.Normal, 1.0f));
+    hit.Normal = glm::vec3(Matrix * glm::vec4(hit.Normal, 0.0f));
     hit.HitDistance = glm::distance(ray.Origin, hit.Position); //Correct for any scaling.
     return true;
 }
